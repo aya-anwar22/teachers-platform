dd88287db50b56e56732afe3947e527ac7ef72f1
aya-anwar22 <aya786930@gmail.com>
2025-07-25 21:49:19 +0300

    backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Server } from 'http';
+
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalPipes(
-  new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }),
-);
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.enableCors({
-  origin: ['http://localhost:4200'], // Angular Dev Server
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-});
+    origin: ['http://localhost:4200'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
