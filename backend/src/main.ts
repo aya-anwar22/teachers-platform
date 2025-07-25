4072d192848383710a9430d4c6cffe4e77fe1fad
aya-anwar22 <aya786930@gmail.com>
2025-07-26 00:17:05 +0300

    fix: build success for vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import express from 'express'; // ✅ كده صح لو عندك esModuleInterop
-import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 const server = express();
+import { ExpressAdapter } from '@nestjs/platform-express';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
