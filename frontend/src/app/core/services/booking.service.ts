5f8d880dca77a4b884dce1b1454ccb1c1163e4e5
ayaanwarse <ayaanwar.se@gmail.com>
2025-07-26 16:40:03 +0300

    Update booking.service.ts

--- frontend/src/app/core/services/booking.service.ts
+++ frontend/src/app/core/services/booking.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Booking } from '../models/booking.model';
 import { Observable } from 'rxjs';
-// import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
