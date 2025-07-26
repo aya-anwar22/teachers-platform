424b905fb91a30824c3920385104e51bbe00c5a1
ayaanwarse <ayaanwar.se@gmail.com>
2025-07-26 12:22:42 +0300

    Update booking.service.ts

--- frontend/src/app/core/services/booking.service.ts
+++ frontend/src/app/core/services/booking.service.ts
@@ -2,13 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Booking } from '../models/booking.model';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment'; 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
-  private apiUrl = 'https://api-noah-aya-anwar-372ab2cd.vercel.app';
-
+  private apiUrl = `${environment.apiUrl}/booking`;
   constructor(private http: HttpClient) { }
 
   createBooking(booking: Booking):Observable<Booking>{
