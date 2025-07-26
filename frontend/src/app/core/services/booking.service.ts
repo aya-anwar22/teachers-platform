809d58777d04b95d04c5fdd72763aa63b30bf0f5
aya-anwar22 <aya786930@gmail.com>
2025-07-26 11:59:37 +0300

    frontend

--- frontend/src/app/core/services/booking.service.ts
+++ frontend/src/app/core/services/booking.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class BookingService {
-  private apiUrl = 'https://api-noah-aya-anwar-372ab2cd.vercel.app';
+  private apiUrl = 'https://teachers-platform-tau.vercel.app';
 
   constructor(private http: HttpClient) { }
 
