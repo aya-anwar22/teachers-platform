f2f340c21d3d7ded2e1ec4e6ff359a1b49c82e12
ayaanwarse <ayaanwar.se@gmail.com>
2025-07-26 16:50:54 +0300

    Update header.component.ts

--- frontend/src/app/frontend/shared/components/header/header.component.ts
+++ frontend/src/app/frontend/shared/components/header/header.component.ts
@@ -10,5 +10,12 @@ export class HeaderComponent {
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    
+    // لمنع التمرير عندما تكون القائمة مفتوحة
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }
 }
