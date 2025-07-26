11c20cb7a941d24e9c211817e4999d0ededcc53a
ayaanwarse <ayaanwar.se@gmail.com>
2025-07-26 13:24:27 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,8 +12,7 @@ export default async function handler(req, res) {
   if (!cachedServer) {
     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-    // ✅ هنا بتحط prefix /api على كل ال routes
-    app.setGlobalPrefix('api');
+
 
     app.useGlobalPipes(
       new ValidationPipe({
